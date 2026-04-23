110a39aa66802ccc68bf9f69393fd722d67c3b39
1Shabarinathreddy <167344268+1Shabarinathreddy@users.noreply.github.com>
2026-04-23 18:53:29 +0100

    fix: replace logging module with clean variable declarations

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,15 +1,3 @@
-# Logging module - placeholder
-variable "log_retention_days" {
-  description = "Number of days to retain logs"
-  type        = number
-  default     = 30
-}
-
-variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
-  default     = {}
-}
 variable "location" {
   description = "Azure region"
   type        = string
