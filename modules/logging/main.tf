2b6c00a760ea9e9a57db802a942edd66f5b95d39
1Shabarinathreddy <167344268+1Shabarinathreddy@users.noreply.github.com>
2026-04-23 18:47:47 +0100

    fix: add variable declarations to logging module
    Add variables for log retention and tags in logging module

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,1 +1,12 @@
 # Logging module - placeholder
+variable "log_retention_days" {
+  description = "Number of days to retain logs"
+  type        = number
+  default     = 30
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
