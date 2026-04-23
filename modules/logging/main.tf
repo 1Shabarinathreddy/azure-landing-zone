48f7a8ecd19035a5e2c032f1117f65756f8c6b7f
1Shabarinathreddy <167344268+1Shabarinathreddy@users.noreply.github.com>
2026-04-23 18:57:00 +0100

    feat: add logging module with log analytics workspace and output

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -25,3 +25,23 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+resource "azurerm_resource_group" "logging" {
+  name     = "rg-logging-${var.environment}"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "law-${var.environment}"
+  location            = azurerm_resource_group.logging.location
+  resource_group_name = azurerm_resource_group.logging.name
+  sku                 = var.log_analytics_workspace_sku
+  retention_in_days   = var.log_retention_days
+  tags                = var.tags
+}
+
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.main.id
+}
