b58d091c14b564515657a4de7935345b4efa0f0c
1Shabarinathreddy <167344268+1Shabarinathreddy@users.noreply.github.com>
2026-05-08 16:38:31 +0100

    fix: separate logging module into correct file structure

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,31 +1,3 @@
-variable "location" {
-  description = "Azure region"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "log_analytics_workspace_sku" {
-  description = "SKU for Log Analytics Workspace"
-  type        = string
-  default     = "PerGB2018"
-}
-
-variable "log_retention_days" {
-  description = "Number of days to retain logs"
-  type        = number
-  default     = 30
-}
-
-variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
-  default     = {}
-}
-
 resource "azurerm_resource_group" "logging" {
   name     = "rg-logging-${var.environment}"
   location = var.location
@@ -33,15 +5,10 @@ resource "azurerm_resource_group" "logging" {
 }
 
 resource "azurerm_log_analytics_workspace" "main" {
-  name                = "law-${var.environment}"
+  name                = "law-${var.environment}-001"
   location            = azurerm_resource_group.logging.location
   resource_group_name = azurerm_resource_group.logging.name
   sku                 = var.log_analytics_workspace_sku
   retention_in_days   = var.log_retention_days
   tags                = var.tags
-}
-
-output "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace"
-  value       = azurerm_log_analytics_workspace.main.id
 }
